aa643f6ffb6e308a2e90a7cdfabe543396656481
adoubt <gerya1508@gmail.com>
2024-12-03 17:22:45 +0700

    база

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from src.handlers import user_handler
+from src.misc import bot, dp
+# from src.methods.database.init_db import init_databases
+
+# Запуск бота
+
+# Инициализация баз данных
+# async def on_startup():
+#     await init_databases()  # Инициализация всех баз данных
+
+def register_handlers():
+    dp.include_routers(user_handler.router)
+
+async def main():
+    # await on_startup()  # Вызов инициализации баз данных
+    register_handlers() # Регистрация обработчиков
+    # aaio_polling_task = asyncio.create_task(payment_polling())  # Отключено, если не нужно
+    
+    # Запускаем все задачи параллельно
+    await asyncio.gather(
+        dp.start_polling(bot),  # Telegram-бот
+
+    )
+if __name__ == "__main__":
+
+
+    asyncio.run(main())
--- src/handlers/user_handler.py
+++ src/handlers/user_handler.py
@@ -2,26 +2,45 @@ import asyncio
 from aiogram import types
 from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message,Location
 
-from src.keyboards import user_keyboards
+
 
 
 router =  Router()
-from src.misc import bot,bot_id
+from src.misc import dp, bot,bot_id
+user_location_data = {}
 
-help_msg = '''/ava {n} Available
-/una {n} Unavailable
-/tak {n} Taken
-n = 10 by default
-/db  Download Base
-/del {username}
-/del all
-/start
-/help
-'''
+def get_user_locations(user_id):
+    return user_location_data.get(user_id, [])
+
+def save_user_locations(user_id, locations):
+    user_location_data[user_id] = locations
+
 
 @router.message(Command("start"))
-@new_user_handler
-async def start_handler(message: Message, state: FSMContext,is_clb=False,**kwargs):
-    await message.answer(help_msg)
+
+async def start_handler(message: Message,is_clb=False,**kwargs):
+    await message.answer("erwtr")
+
+@router.message(F.content_type.in_({'location'}))
+async def handle_route(message: types.Message):
+    if message.location:
+        # Сохранение геолокации отправленной пользователем
+        user_id = message.from_user.id
+        user_locations = get_user_locations(user_id)  # Получаем текущие геолокации пользователя из памяти или базы данных
+        
+        if not user_locations:
+            user_locations = []
+        
+        user_locations.append((message.location.latitude, message.location.longitude))
+        save_user_locations(user_id, user_locations)  # Сохраняем обновленный список геолокаций
+        
+        if len(user_locations) == 2:
+            point_a = user_locations[0]
+            point_b = user_locations[1]
+            await message.answer(f"Маршрут получен: A -> {point_a}, B -> {point_b}")
+            # Очистить список геолокаций после получения маршрута
+            save_user_locations(user_id, [])
+        else:
+            await message.answer("Отправьте геолокацию точки B для завершения маршрута.")
